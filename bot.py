810ff079413abff51f31d62e2ddd689864b9a336
dolitadesse <doilichotadesse200@gmail.com>
2026-08-08 20:14:23 +0300

    Fix auto_reply to check for message text

--- bot.py
+++ bot.py
@@ -128,8 +128,9 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # AUTO REPLY
 # ======================
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not update.message or not update.effective_user:
-        return
+     if not update.message or not update.message.text:
+    return
+
 
     user_id = update.effective_user.id
     text = update.message.text.strip().lower()
